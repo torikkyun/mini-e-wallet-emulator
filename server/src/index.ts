923de3955e4cb2cdf0a78c3a2a71fe152145590d
torikkyun <nduc42176@gmail.com>
2025-08-26 14:17:53 +0700

    Cập nhật database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,26 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { auth } from './lib/auth.js';
+import { cors } from 'hono/cors';
 
-const app = new Hono()
+const app = new Hono();
+
+app.use('*', cors());
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.on(['POST', 'GET'], '/api/auth/*', (c) => {
+  return auth.handler(c.req.raw);
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
